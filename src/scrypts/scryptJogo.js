486f7fa4d10b017e231e2dc07b80e617ef8db706
Romenildo <Romenildoferreira@gmail.com>
2021-12-26 16:15:37 -0300

    colocacao dos navios na tabela somente horizontal

--- src/scrypts/scryptJogo.js
+++ src/scrypts/scryptJogo.js
@@ -16,7 +16,7 @@ const criarTabela = (tipo)=>{
     for(let i = 0; i< tipo; i++){
         tabela+= '<tr linha="'+linhas[i]+'">'
         for(let j = 0; j<tipo; j++){
-             tabela+= '<td coluna="'+colunas[j]+'" title="'+linhas[i]+colunas[j]+' livre"></td>'
+             tabela+= '<td coluna="'+colunas[j]+'" title="'+linhas[i]+colunas[j]+' livre"id="'+linhas[i]+colunas[j]+'"></td>'
          }
          tabela+= '</tr>'
     }
@@ -25,3 +25,57 @@ const criarTabela = (tipo)=>{
 }
 
 criarTabela(10)
+
+const adicionaPortaAviao= (tipo)=>{
+    let posInicial;
+    let posFinal; 
+    let sequencia = []
+    
+    if(posInicial=="" || posFinal==""){
+        //Alerta erro
+    }
+    switch(tipo){
+        case 1:
+            //portaAviao
+            posInicial = document.getElementById('campoPortaAviao1').value.toUpperCase()
+            posFinal = document.getElementById('campoPortaAviao2').value.toUpperCase()
+            break
+        case 2:
+            posInicial = document.getElementById('campoNavioTanque1').value.toUpperCase()
+            posFinal = document.getElementById('campoNavioTanque2').value.toUpperCase()
+            break
+        case 3:
+            posInicial = document.getElementById('campoContratorpedeiros1').value.toUpperCase()
+            posFinal = document.getElementById('campoContratorpedeiros2').value.toUpperCase()
+            break
+        case 4:
+            posInicial = document.getElementById('campoSubmarino1').value.toUpperCase()
+            posFinal = document.getElementById('campoSubmarino2').value.toUpperCase()
+            break
+    }
+
+    sequencia = gerarSequencia(posInicial, posFinal)
+     //fazer verificacao se é aceitavel a posicao
+    for(let pos of sequencia){
+         document.getElementById(pos).style.backgroundColor = "#000"
+    }
+
+}
+
+const gerarSequencia = (posInicial, posFinal) => {
+    posInicial = posInicial.split('')
+    posFinal = posFinal.split('')
+    const letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"]
+
+    let resultado=[];
+    //somente horizontal
+    for(let l of letras){
+        if(l == posInicial[0]){
+            for(let i = posInicial[1]; i<=posFinal[1]; i++){
+                resultado.push(l+i)
+                
+            }
+        }
+    }
+    return resultado;
+}
