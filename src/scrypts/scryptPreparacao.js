820e075b06a6994c9a6df614c4053e597ea07aaf
Romenildo <Romenildoferreira@gmail.com>
2022-01-10 18:45:07 -0300

    sucesso deploy heroku

--- src/scrypts/scryptPreparacao.js
+++ src/scrypts/scryptPreparacao.js
@@ -175,7 +175,6 @@ const voltarEscolherTamanho = () =>{
 //botao iniciar
 
 const iniciarPartida= () =>{
-    iniciarBatalha()
     if(totalEmbarcacoes > 0){
         setAlert(1,"ERRO!!! Todas as Embarcacoes devem ser colocadas")
     }else{
